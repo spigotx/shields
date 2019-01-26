0a7ad2d80139c1c0e2afc3dd6edaed2ac539ca5c
Paul Melnikow <email@paulmelnikow.com>
2019-01-26 18:31:32 -0500

    [DEPLOY] Completely clean the index

--- services/debug/debug.tester.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const Joi = require('joi')
-
-const t = (module.exports = require('..').createServiceTester())
-
-t.create('start time')
-  .get('/starttime.json')
-  .expectJSONTypes({ name: 'start time', value: Joi.date().required() })
